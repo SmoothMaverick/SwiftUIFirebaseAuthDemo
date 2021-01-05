98e543f283b96a51348cb6fb49a1ac81080fa2a5
jason <5660582+SmoothMaverick@users.noreply.github.com>
2021-01-04 16:59:19 -0800

    Initial Commit

---
+++ DemoAuthFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoAuthFirebase
+//
+//  Created by Jason Hoang on 1/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoAuthFirebase/DemoAuthFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAuthFirebaseApp.swift
+//  DemoAuthFirebase
+//
+//  Created by Jason Hoang on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoAuthFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
